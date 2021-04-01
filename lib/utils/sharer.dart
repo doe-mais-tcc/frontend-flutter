fc8276247db0eb339a45b911cb884839628e3796
Nicolas Andalaft <nicolas.andalaft@hotmail.com>
2021-04-01 00:37:36 -0300

    Simplificado método de compartilhamento no facebook

--- lib/utils/sharer.dart
+++ lib/utils/sharer.dart
@@ -1,38 +1,11 @@
 import 'package:url_launcher/url_launcher.dart';
 
 class Sharer {
-  static void shareOnFacebook(String rawUrl) {
-    String convertedUrl = _facebookEncode(rawUrl);
-    String url = 'https://www.facebook.com/sharer/sharer.php?u='
-        '$convertedUrl'
-        ';src=sdkpreparse';
-    canLaunch(url).then(
-      (value) => value
-          ? launch(url)
-          : throw Exception('Não foi possível compartilhar'),
+  static void shareOnFacebook(String link, {String quote}) {
+    String url = 'https://www.facebook.com/sharer/sharer.php?u=$link';
+    if (quote != null) url += '&quote=$quote';
+    launch(url).onError(
+      (error, stackTrace) => throw Exception('Não foi possível compartilhar'),
     );
-  }
-
-  static String _facebookEncode(String url) {
-    const List<String> specialChars = [
-      ':',
-      '/',
-      '#',
-      '?',
-      '&',
-      '@',
-      '%',
-      '+',
-      '~',
-      ';',
-      '=',
-      '\$',
-      ','
-    ];
-    String output = url;
-    for (var char in specialChars) {
-      output.replaceAll(char, char.codeUnits[0].toRadixString(16));
-    }
-    return output;
   }
 }
