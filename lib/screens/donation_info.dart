d266b4e5829b0731f1f8edf6528026a8d966751f
Nicolas Andalaft <nicolas.andalaft@hotmail.com>
2021-04-09 15:50:05 -0300

    Adicionada referência nas informações de doação

--- lib/screens/donation_info.dart
+++ lib/screens/donation_info.dart
@@ -4,8 +4,12 @@ import 'package:doe_mais/components/info_card.dart';
 import 'package:doe_mais/models/info.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DonationInfo extends StatelessWidget {
+  final String referenceUrl =
+      'https://portalarquivos.saude.gov.br/campanhas/doesangue/';
+
   Future<List<Info>> getInfo() async {
     String jsonString =
         await rootBundle.loadString('assets/files/donation_info.json');
@@ -18,17 +22,37 @@ class DonationInfo extends StatelessWidget {
   Widget build(BuildContext context) {
     return AppFrame(
       child: Container(
-        child: FutureBuilder<List<Info>>(
-          future: getInfo(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData)
-              return ListView.builder(
-                itemCount: snapshot.data.length,
-                itemBuilder: (context, index) => InfoCard(snapshot.data[index]),
-              );
+        child: Column(
+          children: [
+            FutureBuilder<List<Info>>(
+              future: getInfo(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData)
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (context, index) =>
+                        InfoCard(snapshot.data[index]),
+                  );
 
-            return Center(child: CircularProgressIndicator());
-          },
+                return Center(child: CircularProgressIndicator());
+              },
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: TextButton(
+                    onPressed: () => launch(referenceUrl),
+                    child: Text('Referência: $referenceUrl'),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
