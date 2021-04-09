94ab1bd800d147b0a0916cba2afded02e01fedda
Nicolas Andalaft <nicolas.andalaft@hotmail.com>
2021-04-09 20:37:57 -0300

    Atualizado layout da campanha do usuário

--- lib/screens/campanha_info.dart
+++ lib/screens/campanha_info.dart
@@ -2,6 +2,7 @@ import 'package:doe_mais/components/app_frame.dart';
 import 'package:doe_mais/components/hemocentro_card.dart';
 import 'package:doe_mais/models/campanha.dart';
 import 'package:flutter/material.dart';
+import 'package:responsively/responsively.dart';
 
 class CampanhaInfo extends StatelessWidget {
   final Campanha campanha;
@@ -14,81 +15,112 @@ class CampanhaInfo extends StatelessWidget {
       menuIndex: 3,
       child: Column(
         children: [
-          Container(
-            constraints: BoxConstraints(maxWidth: 800),
-            child: Column(
-              children: [
-                Text(
-                  'Campanha de ${campanha.nomeInternado}',
-                  style: theme.textTheme.headline1,
+          Text(
+            'Campanha de ${campanha.nomeInternado}',
+            style: theme.textTheme.headline1,
+          ),
+          SizedBox(height: 40),
+          ResponsiveRow(
+            mainAxisSpacing: 20,
+            crossAxisSpacing: 10,
+            children: [
+              ResponsiveColumn(
+                width: ColumnWidth(
+                  smDown: 12,
+                  md: 12,
+                  lgUp: 8,
                 ),
-                SizedBox(height: 30),
-                Card(
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(10, 30, 10, 10),
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(Icons.location_on),
-                            SizedBox(width: 8),
-                            Flexible(
-                              child: Text(
-                                '${campanha.hemocentro.endereco}',
-                                style: theme.textTheme.bodyText1,
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 10),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(Icons.invert_colors),
-                            SizedBox(width: 8),
-                            Flexible(
-                              child: Text(
-                                'Tipo sanguíneo: ${campanha.tipoSanguineo}',
-                                style: theme.textTheme.bodyText1,
-                              ),
-                            ),
-                          ],
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 40, bottom: 20),
-                          child: Text(
-                            'Sobre a campanha',
-                            style: theme.textTheme.headline1,
-                          ),
-                        ),
-                        Container(
-                          constraints: BoxConstraints(minHeight: 100),
-                          child: Text(
-                            '${campanha.descricao}',
-                            style: theme.textTheme.bodyText1,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+                child: card1(theme),
+              ),
+              ResponsiveColumn(
+                width: ColumnWidth(
+                  smDown: 12,
+                  md: 12,
+                  lgUp: 4,
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 30, bottom: 10),
-                  child: Text(
-                    'Essa pessoa está internada aqui',
-                    style: theme.textTheme.headline1,
-                  ),
-                ),
-                ConstrainedBox(
-                  constraints: BoxConstraints(maxWidth: 400),
-                  child: HemocentroCard(campanha.hemocentro),
-                ),
-              ],
-            ),
+                child: card2(theme),
+              ),
+            ],
           ),
         ],
       ),
+    );
+  }
+
+  Column card1(ThemeData theme) {
+    return Column(
+      children: [
+        Text(
+          'Sobre a campanha',
+          style: theme.textTheme.headline1,
+        ),
+        SizedBox(height: 20),
+        Card(
+          child: Container(
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  Text('Campanha criada por ${campanha.user.nome}'),
+                  SizedBox(height: 30),
+                  Wrap(
+                    alignment: WrapAlignment.center,
+                    crossAxisAlignment: WrapCrossAlignment.center,
+                    runSpacing: 10,
+                    children: [
+                      Icon(
+                        Icons.invert_colors,
+                        color: theme.primaryColor,
+                      ),
+                      SizedBox(width: 8),
+                      Text(
+                        'Tipo sanguíneo preferencial: ',
+                        style: theme.textTheme.headline2,
+                      ),
+                      Text(
+                        '${campanha.tipoSanguineo}',
+                        style: theme.textTheme.bodyText2
+                            .copyWith(color: theme.accentColor),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 40, bottom: 20),
+                    child: Text(
+                      'Sobre:',
+                      style: theme.textTheme.headline2,
+                    ),
+                  ),
+                  Container(
+                    constraints: BoxConstraints(minHeight: 100),
+                    child: Text(
+                      '${campanha.descricao}',
+                      style: theme.textTheme.bodyText1,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Column card2(ThemeData theme) {
+    return Column(
+      children: [
+        Text(
+          'Essa pessoa está internada aqui',
+          style: theme.textTheme.headline1,
+        ),
+        SizedBox(height: 20),
+        ConstrainedBox(
+          constraints: BoxConstraints(maxWidth: 500),
+          child: HemocentroCard(campanha.hemocentro),
+        ),
+      ],
     );
   }
 }
